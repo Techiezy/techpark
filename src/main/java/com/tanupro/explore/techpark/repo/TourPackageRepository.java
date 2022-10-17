f30c3bf2e27644f94e2e230d52d2a13a3ba07d09
tanu70998 <tanu70998@gmail.com>
2022-10-18 00:27:08 +0530

    repo <TourRepository,TourPackageRespository> CrudRepository

---
+++ src/main/java/com/tanupro/explore/techpark/repo/TourPackageRepository.java
@@ -0,0 +1,7 @@
+package com.tanupro.explore.techpark.repo;
+
+import com.tanupro.explore.techpark.model.TourPackage;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TourPackageRepository extends CrudRepository<TourPackage,String> {
+}
---
+++ src/main/java/com/tanupro/explore/techpark/repo/TourRepository.java
@@ -0,0 +1,7 @@
+package com.tanupro.explore.techpark.repo;
+
+import com.tanupro.explore.techpark.model.Tour;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TourRepository extends CrudRepository<Tour,Integer> {
+}
