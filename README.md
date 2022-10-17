0ad1558296c1aca0b20de6ab6808015c28e11e2a
tanu70998 <tanu70998@gmail.com>
2022-10-17 23:48:53 +0530

    model <Region,Difficulty,tour,tourpackage>

---
+++ README.md
@@ -0,0 +1,2 @@
+1) ```./mvnw package```
+2) ```java -Dserver.port=9090 -jar target/techpark-0.0.1-SNAPSHOT.jar```
---
+++ src/main/java/com/tanupro/explore/techpark/model/Difficulty.java
@@ -0,0 +1,5 @@
+package com.tanupro.explore.techpark.model;
+
+public enum Difficulty {
+    Easy, Medium, Difficult, Varies
+}
